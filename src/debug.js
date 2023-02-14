fac6eb84d3689cc8634422f9e9f437ca2a16dd1d
thesneilert <eilert.gaming@outlook.com>
2023-02-14 14:13:44 +0100

    fix

--- src/debug.js
+++ src/debug.js
@@ -1,8 +1,8 @@
 
 
 //use this to see what room your in to debug
-showDebugWindow() //turn this on or off to view debug window
-function showDebugWindow() {
+debugWindow() //turn this on or off to view debug window
+function debugWindow() {
     appDebug.innerHTML = /*html*/`
     The character postion is ${playerPosition.x}x,${playerPosition.y}y.<br>
     <p style="font-size: 12px; color:#569459;">This window is intended for debugging
