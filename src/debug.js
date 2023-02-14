fe48a0fe83b81753bdc576afead61a96a092b927
thesneilert <eilert.gaming@outlook.com>
2023-02-14 14:02:34 +0100

    Added debugwindow

---
+++ src/debug.js
@@ -0,0 +1,11 @@
+
+
+//use this to see what room your in to debug
+showDebugWindow() //turn this on or off to view debug window
+function showDebugWindow() {
+    appDebug.innerHTML = /*html*/`
+    The character postion is ${playerPosition}.<br>
+    <p style="font-size: 12px; color:#569459;">This window is intended for debugging
+    <br>and is not a part of the final product.</p>
+    `;
+}
