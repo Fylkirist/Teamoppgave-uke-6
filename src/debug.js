3ef01750600d79ffe754d57f3003a0de0e2d59c5
thesneilert <eilert.gaming@outlook.com>
2023-02-15 11:41:50 +0100

    debug fix

--- src/debug.js
+++ src/debug.js
@@ -5,7 +5,8 @@ debugWindow() //turn this on or off to view debug window
 function debugWindow() {
     appDebug.innerHTML = /*html*/`
     The character postion is ${playerPosition.x}x,${playerPosition.y}y.<br>
-    Current car type is ${carPosition.type}, and it's id is ${cars[carPosition.type][carPosition.id]}.<br>
+    Current side car type is ${carPosition.type}. <br>
+    The id for this car is ${cars[carPosition.type][carPosition.id]}.<br>
     <p style="font-size: 12px; color:#569459;">This window is intended for debugging
     <br>and is not a part of the final product.</p>
     `;
