e5243bc21b522adbc619f9afef2d190cfc33dc76
thesneilert <eilert.gaming@outlook.com>
2023-02-16 14:48:05 +0100

    change

--- src/model.js
+++ src/model.js
@@ -1,6 +1,6 @@
 var playerPosition = {x:200,y:250};
 let carPosition = {x:395, y:-120, id:0, type:0}; // posisjon på bro bil
-let propPosition = {x:60, y:0, id:0, type:0}; // posisjon på prop
+let propPosition = {x:80, y:0, id:0, type:0}; // posisjon på prop
 const roadGrid = {x:80,y:10, width: 267, height: 380}
 var score = 0;
 var health = 100;
